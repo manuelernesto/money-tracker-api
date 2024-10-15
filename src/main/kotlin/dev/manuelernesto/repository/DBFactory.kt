5be5b0627cf39508892cf6c8b14384e813e8304a
manuelernesto <alemdadox@gmail.com>
2024-10-15 20:38:16 +0100

    🌱| improve error handling

--- src/main/kotlin/dev/manuelernesto/repository/DBFactory.kt
+++ src/main/kotlin/dev/manuelernesto/repository/DBFactory.kt
@@ -6,7 +6,7 @@ import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import org.jetbrains.exposed.sql.transactions.transaction
-import io.ktor.server.application.*
+
 /**
  * @author  Manuel Ernesto (manuelernest0)
  * @date  14/10/24 7:33 PM
